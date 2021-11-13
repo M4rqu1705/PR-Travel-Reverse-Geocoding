f60529b802f0e5e6518020035faa586e6315c91e
M4rqu1705 <m4rc05.dev@gmail.com>
2021-11-13 09:14:57 -0400

    Using a different API works!!

--- scripts/script.js
+++ scripts/script.js
@@ -8,20 +8,26 @@ function run(location) {
 
   output.innerText = latitude;
   output.innerText += ", " + longitude;
-  output.innerText += "(" + accuracy + ")\n";
+  // output.innerText += " (" + accuracy + ")\n";
 
-  let reverseGeocoder = new BDCReverseGeocode();
+  fetch("https://nominatim.openstreetmap.org/reverse?format=json&lat=18.4030875&lon=-65.9637902&zoom=18&addressdetails=1")
+    .then(res => res.json())
+    .then((geocoded) => {
+      const city = geocoded.address.city;
+      const city_district = geocoded.address.city_district;
+      const country = geocoded.address.country;
+      const country_code = geocoded.address.city_district;
+      const county = geocoded.address.county;
+      const neighbourhood = geocoded.address.neighbourhood;
+      const postcode = geocoded.address.postcode;
+      const road = geocoded.address.road;
+      const display_name = geocoded.display_name;
 
-  reverseGeocoder.getClientLocation({
-    latitude: -latitude,
-    longitude: longitude,
-  }, function (result) {
-    output.innerText += result.locality;
-  });
+      output.innerText += municipality + ", " + city;
 
-
-
-
+    }).catch(err => {
+      error_output = "Error: " + err.message;
+    })
 }
 
 function main() {
