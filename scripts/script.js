3bf6582e8695597f9e7f6dd86c213cb29f4fa79b
M4rqu1705 <m4rc05.dev@gmail.com>
2021-11-14 00:17:25 -0400

    Can now change theme using prettier icon

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,43 @@
-let coordinates = document.getElementById("coordinates");
-let summary = document.getElementById("summary");
-let display_name = document.getElementById("display-name");
-let error_output = document.getElementById("error");
-let light_theme = true;
+const coordinates = document.getElementById("coordinates");
+const summary = document.getElementById("summary");
+const display_name = document.getElementById("display-name");
+const error_output = document.getElementById("error");
+const theme_image = document.querySelector("#theme > img");
 
+// Receives current theme and updates CSS variables accordingly.
+function setTheme(theme) {
+  const r = document.querySelector(':root');
+
+  if (theme === 'light') {
+    r.style.setProperty('--foreground-color', '#2c2c2c');
+    r.style.setProperty('--background-color', 'whitesmoke');
+    theme_image.src = "./assets/change-theme-icon-dark.png";
+
+  } else {
+    r.style.setProperty('--foreground-color', 'whitesmoke');
+    r.style.setProperty('--background-color', '#2c2c2c');
+    theme_image.src = "./assets/change-theme-icon-light.png";
+  }
+}
+
+// Changes theme from dark to light or vice-versa.
+function toggleTheme() {
+  // Retrieve current theme
+  let theme = localStorage.getItem("light-theme");
+
+  // Toggle it
+  if (theme === 'dark') theme = 'light';
+  else theme = 'dark';
+
+  // Store it
+  localStorage.setItem("light-theme", theme);
+
+  // Update theme
+  setTheme(theme);
+}
+
+
+// Receive location and update screen accordingly
 function run(location) {
   const latitude = location.coords.latitude;
   const longitude = location.coords.longitude;
@@ -11,7 +45,8 @@ function run(location) {
 
   coordinates.innerText = latitude + ", " + longitude;
 
-  fetch("https://nominatim.openstreetmap.org/reverse?format=json&lat=" + latitude + "&lon=" + longitude + "&zoom=18&addressdetails=1")
+  const url = "https://nominatim.openstreetmap.org/reverse?format=json&lat=" + latitude + "&lon=" + longitude + "&zoom=18&addressdetails=1";
+  fetch(url)
     .then(res => res.json())
     .then((geocoded) => {
       const city = geocoded.address.city;
@@ -32,6 +67,7 @@ function run(location) {
     })
 }
 
+// 
 function main() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(run, (err) => {
@@ -51,23 +87,3 @@ window.addEventListener("load", () => {
   main();
   setInterval(main, 30000);
 });
-
-function toggleTheme() {
-  let theme = localStorage.getItem("light-theme");
-  if (theme === 'dark') theme = 'light';
-  else theme = 'dark';
-  localStorage.setItem("light-theme", theme);
-  setTheme(theme);
-}
-
-function setTheme(theme) {
-  const r = document.querySelector(':root');
-
-  if (theme === 'light') {
-    r.style.setProperty('--foreground-color', '#2c2c2c');
-    r.style.setProperty('--background-color', 'whitesmoke');
-  } else {
-    r.style.setProperty('--foreground-color', 'whitesmoke');
-    r.style.setProperty('--background-color', '#2c2c2c');
-  }
-}
